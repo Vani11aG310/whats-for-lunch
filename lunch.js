2e185c47f69479550566f8eb0b9016451bd69bee
Angelo <angvil02@gmail.com>
2024-10-14 12:51:29 -0700

    functionality added to whatToDoForLunch

--- lunch.js
+++ lunch.js
@@ -1,27 +1,19 @@
-/*
- * Modify the contents of the function below, such that:
- *
- * If we're not hungry, we want to tell ourselves to get back to work.
- * Otherwise, we want to pick something up and eat it in the lab when
- * we've got less than 20 minutes or to try a place nearby if we've
- * got between 20 and 30 minutes. If we have any more time than that,
- * we want to remind ourselves that we're in a bootcamp and that we
- * should reconsider how much time we actually have to spare.
- *
- * hungry is a Boolean, representing if you're hungry or not.
- * availableTime is a Number representing the time you have for lunch,
- * in minutes.
- */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
+  // check if we are not hungry
+  if (!hungry) {
+    console.log("I should get back to work.")
+  } else {
+    // check how much time I have to eat
+    if (availableTime < 20) {
+      console.log("I should pick something up and eat it in the lab.")
+    } else if (availableTime >= 20 && availableTime <= 30) {
+      console.log("I can try to find a nice place to eat nearby.")
+    } else {
+      console.log("Remember, I am in the bootcamp now, I should try to see if I can get more work done before I grab something to eat.")
+    }
+  }
 }
-
-
-/*
- * This is some test runner code that's simply calling our whatToDoForLunch function
- * defined above to verify we're making the right decisions. Do not modify it!
- */
 
 console.log("I'm hungry and I have 20 minutes for lunch.");
 whatToDoForLunch(true, 20);
