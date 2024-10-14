dd409c3019c8096de1be227ea4316faafb50ce35
Angelo <angvil02@gmail.com>
2024-10-14 13:02:43 -0700

    cleaned code to abide by eslint suggestions

--- lunch.js
+++ lunch.js
@@ -2,18 +2,18 @@
 const whatToDoForLunch = function(hungry, availableTime) {
   // check if we are not hungry
   if (!hungry) {
-    console.log("I should get back to work.")
+    console.log("I should get back to work.");
   } else {
     // check how much time I have to eat
     if (availableTime < 20) {
-      console.log("I should pick something up and eat it in the lab.")
+      console.log("I should pick something up and eat it in the lab.");
     } else if (availableTime >= 20 && availableTime <= 30) {
-      console.log("I can try to find a nice place to eat nearby.")
+      console.log("I can try to find a nice place to eat nearby.");
     } else {
-      console.log("Remember, I am in the bootcamp now, I should try to see if I can get more work done before I grab something to eat.")
+      console.log("Remember, I am in the bootcamp now, I should try to see if I can get more work done before I grab something to eat.");
     }
   }
-}
+};
 
 console.log("I'm hungry and I have 20 minutes for lunch.");
 whatToDoForLunch(true, 20);
